f1d90067f80f8cab950090811d0dcea514650c44
Kesava Raj <kesavarajtry@gmail.com>
2022-04-30 15:46:56 +0530

    your commit message

---
+++ routes/api.js
@@ -0,0 +1,91 @@
+
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const db =
+    "mongodb+srv://pcyogesh:pcyogesh@cluster0.3ni4w.mongodb.net/billbook?retryWrites=true&w=majority";
+mongoose.connect(db, (err) => {
+    if (err) {
+        console.log("Error !" + err);
+    } else {
+        console.log("connected to mongoDB");
+    }
+});
+
+// Models
+const Borrower = require("../Models/borrower");
+
+// add borrower - POST
+
+router.post("/addBorrower", (req, res) => {
+    let borrower = new Borrower(req.body);
+    borrower.save((error, borrower) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send({
+                status: true,
+                msg: "OK",
+                borrower,
+            });
+        }
+    });
+});
+
+// Get borrowers 
+
+router.get("/borrowersList", (req, res) => {
+    Borrower.find(function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+        .sort({ createdDate: -1 });
+});
+
+// save transaction 
+
+router.post("/saveTransaction", (req, res) => {
+    Borrower.findByIdAndUpdate(
+        req.body.userId,
+        {
+            $push: {
+                transactions:{
+                    dueDate:req.body.dueDate,
+                    dueAmount:req.body.dueAmount
+                }
+            },
+        },
+        {
+            new: true,
+        },
+        function (err, result) {
+            if (err) {
+                res.send("Error updating user");
+            } else {
+                res.status(200).send({
+                    status: true,
+                    msg: "OK!",
+                    result,
+                });
+            }
+        }
+    );
+});
+
+// GET all transactions
+
+router.post("/getLatestTransactions", (req, res) => {
+    Borrower.find({'_id':req.body._id},function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    }).sort({ dueDate: -1 });
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+//express package
+const express = require('express');
+//middleware package
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const api = require('./routes/api');
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(cors());
+app.use('/api', api);
+// parse application/x-www-form-urlencoded
+
+app.use(function (req, res, next) {
+  //set headers to allow cross origin request.
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.get('/api', function (req, res) {
+  res.end('file catcher examplehh');
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
+console.log('server running on port', 5000);
